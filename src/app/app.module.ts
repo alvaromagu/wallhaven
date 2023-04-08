21fa97ff75589c698ada094191b8315337c307ee
Alvaro <alvaromagu2002@gmail.com>
2023-04-08 19:07:57 +0200

    Add dark theme and spinner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './components/home/home.component';
+import {HttpClientModule} from "@angular/common/http";
+import { SpinnerComponent } from './components/spinner/spinner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    SpinnerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {WallhavenService} from "../../services/wallhaven.service";
+import {finalize, take} from "rxjs";
+import {Daum} from "../../types/wallhaven-search";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  daums: Daum[] = [];
+  loading = false;
 
+  constructor(
+    private wallhavenService: WallhavenService
+  ) {}
+
+  ngOnInit(): void {
+    this.loadThumbs();
+  }
+
+  loadThumbs() {
+    this.loading = true;
+    this.wallhavenService.search().pipe(
+      take(1),
+      finalize(() => this.loading = false)
+    ).subscribe({
+      next: value => {
+        this.daums = value.data;
+      },
+      error: err => {
+        console.log(err);
+      }
+    });
+  }
 }
